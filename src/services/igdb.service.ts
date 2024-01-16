96a56d25b9c1252359a389ea7417f2612576f87d
italloVidal <itallovidalll@gmail.com>
2024-01-16 15:21:15 -0300

    add auth with axios

--- src/services/igdb.service.ts
+++ src/services/igdb.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class IGDBService {
-  async auth() {
+  private async auth() {
     const response = await fetch(
       'https://id.twitch.tv/oauth2/token?client_id=2ty2mja5wgqlcu4uqzvcfrci2r6izl&client_secret=7ckqi2eeezh4cjahqfz90p9x707vbd&grant_type=client_credentials',
       {
@@ -11,22 +11,30 @@ export class IGDBService {
       },
     )
     const authObject: { access_token: string } = await response.json()
-    return authObject.access_token
+
+    return axios.create({
+      baseURL: `https://api.igdb.com/v4`,
+      headers: {
+        'Content-Type': 'text/plain',
+        Authorization: `Bearer ${authObject.access_token}`,
+        'Client-ID': `2ty2mja5wgqlcu4uqzvcfrci2r6izl`,
+      },
+    })
   }
 
   // pega um número aleatório
-  getRandomInt(min: number, max: number) {
+  private getRandomInt(min: number, max: number) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
   // verifica se o objeto é vazio
-  isEmptyObject(obj) {
+  private isEmptyObject(obj) {
     return !Object.keys(obj).length
   }
 
-  async getCount(api: AxiosInstance) {
+  private async getCount(api: AxiosInstance) {
     try {
       const response = await api.post('/games/count', 'where rating > 80;')
 
@@ -36,7 +44,7 @@ export class IGDBService {
     }
   }
 
-  async getIGDBGame(offset, api: AxiosInstance) {
+  private async getIGDBGame(offset, api: AxiosInstance) {
     try {
       console.log(offset)
       const query = `fields *; limit 1; offset ${offset};`
@@ -49,20 +57,12 @@ export class IGDBService {
 
   async getRandomGame() {
     try {
-      const token = await this.auth()
-      const IGBBApi = axios.create({
-        baseURL: `https://api.igdb.com/v4`,
-        headers: {
-          'Content-Type': 'text/plain',
-          Authorization: `Bearer ${token}`,
-          'Client-ID': `2ty2mja5wgqlcu4uqzvcfrci2r6izl`,
-        },
-      })
-
+      const IGBBApi = await this.auth()
       const { count } = await this.getCount(IGBBApi)
+
       let offset = this.getRandomInt(1, count)
       let game = await this.getIGDBGame(offset, IGBBApi)
-      console.log(game)
+
       while (this.isEmptyObject(game)) {
         offset = this.getRandomInt(1, count)
         game = await this.getIGDBGame(offset, IGBBApi)
